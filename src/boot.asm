d5943d4e86793c20f595d7068603788411834638
derbuihan <derbuihan@gmail.com>
2024-08-24 15:59:28 +0900

    Bios parameter block

--- src/boot.asm
+++ src/boot.asm
@@ -1,7 +1,16 @@
 ORG 0
 BITS 16
 
+_start:
+    jmp short start
+    nop
+
+times 33 db 0
+
 start:
+    jmp 0x7c0:step2
+
+step2:
     cli; Disable interrupts
     ; Set data segment to 0x7c0
     mov ax, 0x7c0
