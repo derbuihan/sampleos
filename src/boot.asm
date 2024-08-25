778e2a3d3ee584e71e879b079bf4f64597ed38f7
derbuihan <derbuihan@gmail.com>
2024-08-25 13:07:45 +0900

    protected mode

--- src/boot.asm
+++ src/boot.asm
@@ -1,6 +1,9 @@
 ORG 0
 BITS 16
 
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start:
     jmp short start
     nop
@@ -8,64 +11,66 @@ _start:
 times 33 db 0
 
 start:
-    jmp 0x7c0:step2
+    jmp 0:step2
 
 step2:
     cli; Disable interrupts
-    ; Set data segment to 0x7c0
-    mov ax, 0x7c0
+    mov ax, 0x00 ; Set data segment to 0x0
     mov ds, ax
     mov es, ax
-    ; Set stack segment to 0x0000
-    mov ax, 0x00
     mov ss, ax
     mov sp, 0x7c00 ; Set stack pointer to 0x7c0
     sti; Enable interrupts
 
-    ; Load the second sector
-    mov ah, 2 ; Read sectors
-    mov al, 1 ; Read one sector
-    mov ch, 0 ; Cylinder 0
-    mov cl, 2 ; Read second sector
-    mov dh, 0 ; Head number
-    mov bx, buffer ; Buffer address
-    int 0x13 ; BIOS interrupt
+.load_protected:
+    cli
+    lgdt [gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
 
-    jc error
+; GDT
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
 
-    mov si, message
-    call print
+; Offset 0x08
+gdt_code:     ; CS SHOULD BE POINT TO THIS
+    dw 0xffff ; Segment Limit first 0-15 bits
+    dw 0x0    ; Base first 0-15 bits
+    db 0x0    ; Base 16-23 bits
+    db 0x9a   ; Access Byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0x0    ; Base 24-31 bits
 
-    mov si, buffer
-    call print
+; Offset 0x10
+gdt_data:     ; DS, SS, ES, FS, GS
+    dw 0xffff ; Segment Limit first 0-15 bits
+    dw 0x0    ; Base first 0-15 bits
+    db 0x0    ; Base 16-23 bits
+    db 0x92   ; Access Byte
+    db 11001111b ; High 4 bit flags and the low 4 bit flags
+    db 0x0    ; Base 24-31 bits
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
     jmp $
-
-error:
-    mov si, error_message
-    call print
-    jmp $
-
-print:
-    mov bx, 0
-.loop:
-    lodsb
-    cmp al, 0
-    je .done
-    call print_char
-    jmp .loop
-.done:
-    ret
-
-print_char:
-    mov ah, 0eh
-    int 10h
-    ret
-
-message db "Hello, Kernel!", 0
-
-error_message db "Error loading the second sector", 0
 
 times 510-($-$$) db 0
 dw 0xAA55
-
-buffer:
